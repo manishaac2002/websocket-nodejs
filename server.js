bdf41d9e6943c79737a386617725ad9e48096132
manishaac2002 <manishaac2002@gmail.com>
2023-11-29 14:13:44 +0530

    server.js

--- server.js
+++ server.js
@@ -4,26 +4,26 @@ const express = require("express")
 const http = require("http")
 const socket = require("socket.io")
 
-const app=express()
-const server=http.createServer(app)
-const io=socket(server)
+const app = express()
+const server = http.createServer(app)
+const io = socket(server)
 
-let users =[]
+let users = []
 
 // channels 
-const message ={
-    general:[],
-    Random:[],
-    jokes:[],
-    javascript:[]
+const message = {
+    general: [],
+    Random: [],
+    jokes: [],
+    javascript: []
 }
 
 // user connection established
-io.connection('',()=>{
-    socket.on("join users",(username)=>{
-        const user ={
+io.on('connection', () => {
+    socket.on("join users", (username) => {
+        const user = {
             username,
-            id:socket.id
+            id: socket.id
         }
         user.push(user)
         io.emit("new user,user")
@@ -31,14 +31,43 @@ io.connection('',()=>{
     })
 
     // call back 
-    socket.on("join room",(roomName,callBack)=>{
+    socket.on("join room", (roomName, callBack) => {
         socket.join(roomName)
         callBack(message[roomName])
     })
 
+    socket.on("send message", ({ content, to, sender, chatName, isChannel }) => {
+        if (isChannel) {
+            const payload = {
+                content,
+                chatName,
+                sender
+            }
+            socket.to(to).emit("new message", payload)
+        }
+        else {
+            const payload = {
+                content,
+                chatName: sender,
+                sender
+            }
+            socket.to(to).emit("new message", payload)
+        }
+        if(message[chatName]){
+            message[chatName].push({
+                sender,
+                content
+            })
+        }
+    })
+    //disconnection
+    socket.on('disconnection',()=>{
+        users=users.filter(u=>u.id !== socket.id)
+        io.emit('new users',users)
+    })
 })
 
 // port
-server.listen(2000,()=>{
+server.listen(2000, () => {
     console.log("Server is running on port");
 })
